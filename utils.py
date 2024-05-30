4cf64a5084b5fd4ce36a5a0da06318f5f0774926
Chitranshi <chitranshi@zib.de>
2024-05-30 09:30:02 +0200

    Add MinMaxScaler for Pytorch

--- utils.py
+++ utils.py
@@ -20,3 +20,33 @@ def adv_loss(lamb,
     dist_loss = L1_MAD_weighted(x_pert,x_orig)
     
     return (sq_diff + dist_loss).mean()
+
+class MinMaxScalerTorch:
+
+    def __init__(self, X) -> None:
+        
+        self.X = X
+        self.mean = None
+        self.max = None
+        self.min = None
+
+    def fit(self):
+        self.min = self.X.min(dim=0).values
+        self.max = self.X.max(dim=0).values
+        self.mean = self.X.mean(dim=0)
+
+    def transform(self, X):    
+        assert self.min != None, "Scaler not fitted"
+        assert self.max != None, "Scaler not fitted"
+        assert self.mean != None, "Scaler not fitted" 
+    
+        XScaled = (X - self.min) / (self.max - self.min)
+        return XScaled
+
+    def inverseTransform(self, XScaled):
+        assert self.min != None, "Scaler not fitted"
+        assert self.max != None, "Scaler not fitted"
+        assert self.mean != None, "Scaler not fitted" 
+
+        XUnscaled = XScaled * (self.max - self.min) + self.min
+        return XUnscaled
